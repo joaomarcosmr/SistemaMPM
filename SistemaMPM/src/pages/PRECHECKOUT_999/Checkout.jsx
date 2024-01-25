747bd2f2dc443eba365075ad10a4fd408117ee08
joaomarcosmr <101360915+joaomarcosmr@users.noreply.github.com>
2024-01-25 19:41:13 -0300

    commit

--- SistemaMPM/src/pages/PRECHECKOUT_999/Checkout.jsx
+++ SistemaMPM/src/pages/PRECHECKOUT_999/Checkout.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div className='containerEtapa'>
+        <div className='text-center'>
+            <h2>Meu Primeiro Milhão</h2>
+        </div>
+        <div className="resultados">
+            <div className='resultados-titulo'>
+                <div className='antes'>
+                    <h3 className='text-center'>ANTES</h3>
+                </div>
+                <div className='depois'>
+                    <h3 className='text-center'>DEPOIS</h3>
+                </div>
+            </div>
+            <div className='resultados-imagem'>
+                <div className='antes'>
+
+                </div>
+                <div className='depois'>
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
